7e2011274b22cf2710fbbb8e1c6f9df8ad052241
henrylin2008 <henrylin2008@yahoo.com>
2019-06-03 06:45:07 -0700

    api update

--- yelp_api.py
+++ yelp_api.py
@@ -8,16 +8,46 @@ token_secret = ''
 
 auth = OAuth1(consumer_key, consumer_secret,
               token, token_secret)
+#
+# url = "https//api.yelp.com/v2/search"
+#
+# params = {
+#     "term": "food",
+#     "location": "San Francisco",
+#
+# }
+#
+#
+# r = requests.get(url, auth=auth, params=params)
+#
+# print(r.text)
 
-url = "https//api.yelp.com/v2/search"
+def do_search(term='Food', location='San Francisco'):
+    base_url = 'https://api.yelp.com/v2/search'
+    term = term.replace(" ", "+")
+    location = location.replace(" ", "+")
+    url = "{base_url}?term={term}&location={location}".format(
+            base_url=base_url,
+            term=term,
+            location = location)
+    )
 
-params = {
-    "term": "food",
-    "location": "San Francisco",
+    params ={
+        "term": term,
+        "location": location
+    }
+    auth = OAuth1(consumer_key, consumer_secret,
+              token, token_secret)
 
-}
+    r = requests.get(url, auth=auth, params=params)
+    return r.json()
 
+search1 = do_search()
 
-r = requests.get(url, auth=auth)
-
-print(r.text)
+for i in search1["businesses"]:
+    print(i["name"])
+    print(i["phone"])
+    print(i["location"]["display_address"])
+    print(i["location"]["city"])
+    print(i.get('location').get('area'))
+    print('\n')
